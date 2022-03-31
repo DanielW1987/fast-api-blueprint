e4b8a081e1824d3d6df2849ac603f5a2d9f2004a
Daniel Wagner <wagner.daniel87@gmail.com>
2022-03-31 18:02:46 +0200

    check format gh action

--- app/core/config.py
+++ app/core/config.py
@@ -42,13 +42,7 @@ class Settings(BaseSettings):
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-        )
+        return PostgresDsn.build(scheme="postgresql", user=values.get("POSTGRES_USER"), password=values.get("POSTGRES_PASSWORD"),host=values.get("POSTGRES_SERVER"),path=f"/{values.get('POSTGRES_DB') or ''}",)
 
     SMTP_TLS: bool = True
     SMTP_PORT: Optional[int] = None
